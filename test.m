1ebd747425ab91a6469bedb40852215caa7a5c27
HaurHaur <haur0710@gmail.com>
2023-12-04 22:35:14 +0800

    Add test.m for unit testing

---
+++ test.m
@@ -0,0 +1,29 @@
+classdef test
+    %TEST Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        Property1
+    end
+    
+    methods(Static)
+        function test_encoder()
+            %TEST Construct an instance of this class
+            %   Detailed explanation goes here
+        end
+        
+        function test_decoder()
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+        end
+
+        function test_encryption()
+
+        end
+
+        function test_dataHiding()
+            
+        end
+    end
+end
+
