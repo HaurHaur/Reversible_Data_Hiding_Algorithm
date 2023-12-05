9fb5405ae22b00d56c285ba736cdfea24f3a2921
HaurHaur <haur0710@gmail.com>
2023-12-05 22:52:59 +0800

    Implement constructer of Encoder

--- Encoder.m
+++ Encoder.m
@@ -4,21 +4,59 @@ classdef Encoder
     
     properties
         originalImage
-        keyImage
+        imageKey
+        embeddingKey
+        embeddingData
     end
     
-    methods
-        function obj = Encoder(inputArg1,inputArg2)
+    methods(Static)
+        function obj = Encoder()
             %ENCODER Construct an instance of this class
             %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+            ORG = Image(Encoder.loadImage());
+            obj.originalImage = ORG;
+            key = Encoder.generateKey(ORG.Nr, ORG.Nc, 8);
+            obj.imageKey = Image(key);
+            obj.embeddingData = Encoder.loadEmbedding();
+            obj.embeddingKey = Encoder.generateKey(1, ORG.Nr*ORG.Nc*0.75, 1);
         end
         
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        function ORG = loadImage()
+            [filename,pathname]=uigetfile({'*.bmp; *.tif','(*.bmp);(*.tif)';},'打開圖片');
+            if isequal(filename,0)
+               disp('Image selection cancelled.');
+               return;
+            else
+               disp(['Image selected: ', fullfile(pathname, filename)])
+            end
+            
+            IMG=imread([pathname,filename]);  
+            DIM=size(IMG);
+            if length(DIM)==3
+                ORG=rgb2gray(IMG);
+            elseif length(DIM)==2
+                ORG=IMG;
+            else
+                disp('Image Loading error.');
+            end
+        end
+
+        function EMB = loadEmbedding()
+            fid=fopen('book.txt');
+            data=98304;
+            emb=fread(fid,data,'ubit1');
+            EMB=[emb',emb'];
+            fclose(fid);
+        end
+
+        function key = generateKey(Nr, Nc, Nb)
+            key = reshape(uint8(round(rand(1, Nr*Nc)*(2^Nb-1))), Nr, Nc)
         end
     end
+
+    methods
+        
+    end
+
 end
 
--- Image.m
+++ Image.m
@@ -1,10 +1,16 @@
 classdef Image
     properties
         image;
-        heigt;
-        weight;
+        Nr;
+        Nc;
+        pixels;
     end
     methods
+        function obj = Image(img)
+            obj.image = img;
+            [obj.Nr, obj.Nc] = size(img);
+            obj.pixels = obj.Nr * obj.Nc
+        end
         function subImage = getSubImage(img, index)
             subImage = img.image(abs(mod(index,2)-2):2:end,floor(index/2)+1:2:end);
         end
