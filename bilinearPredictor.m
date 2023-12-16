3aea49517dc083ad06786005fa7ca8acf95a71f5
HaurHaur <haur0710@gmail.com>
2023-12-16 15:12:51 +0800

    Implement bilinear predictor

---
+++ bilinearPredictor.m
@@ -0,0 +1,23 @@
+classdef bilinearPredictor < Predictor
+    %BILINEARPREDICTOR Summary of this class goes here
+    %   Detailed explanation goes here
+       
+    methods        
+        function result = apply(enc)
+           [Nc, Nr] = size(enc);
+            x = enc(1:2:end,1:2:end); x2 = enc(2:2:end,1:2:end);
+            x3 = enc(1:2:end,2:2:end); x4 = enc(2:2:end,2:2:end);
+
+            z = imresize(x,2,'bilinear');
+            z(1:2:end,1:2:end)=x;
+            z1 = z(1:2:end,1:2:end); z2 = z(2:2:end,1:2:end);
+            z3 = z(1:2:end,2:2:end); z4 = z(2:2:end,2:2:end);
+
+            result = uint8(zeros(Nr,Nc));
+            result(1:2:end,1:2:end) = z1; 
+            y2 = pred(z2,x2); y3 = pred(z3,x3); y4 = pred(z4,x4);
+            result(2:2:end,1:2:end) = y2; result(1:2:end,2:2:end) = y3; result(2:2:end,2:2:end) = y4;            
+        end
+    end
+end
+
