db6921fb04de385a8a3d3b418a93737550721740
HaurHaur <haur0710@gmail.com>
2023-12-21 20:24:17 +0800

    Refactor bilinearPredictor.

--- bilinearPredictor.m
+++ bilinearPredictor.m
@@ -7,12 +7,10 @@ classdef bilinearPredictor < Predictor
            [Nc, Nr] = size(enc);
             x = enc(1:2:end,1:2:end); x2 = enc(2:2:end,1:2:end);
             x3 = enc(1:2:end,2:2:end); x4 = enc(2:2:end,2:2:end);
-
             z = imresize(x,2,'bilinear');
             z(1:2:end,1:2:end)=x;
             z1 = z(1:2:end,1:2:end); z2 = z(2:2:end,1:2:end);
             z3 = z(1:2:end,2:2:end); z4 = z(2:2:end,2:2:end);
-
             result = uint8(zeros(Nr,Nc));
             result(1:2:end,1:2:end) = z1; 
             y2 = Predictor.errorCorrection(z2,x2); y3 = Predictor.errorCorrection(z3,x3); y4 = Predictor.errorCorrection(z4,x4);
