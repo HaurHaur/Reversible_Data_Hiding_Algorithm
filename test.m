d85a0c093d3956e37cd759b0f44d8b369cfcfd4e
HaurHaur <haur0710@gmail.com>
2023-12-21 20:07:49 +0800

    Add new test case to test dpcm algorithm.

--- test.m
+++ test.m
@@ -5,31 +5,39 @@ classdef test
     end
     
     methods(Static)
-        function [result, key1, key2] = test_encoder()
+        function [result, key1, key2, coefficient] = test_encoder()
             encrypter = xorEncrypter;
             encoder = Encoder(encrypter);
-            [result, key1, key2] = encoder.apply();
+            [result, key1, key2, coefficient] = encoder.apply();
         end
         
-        function test_decoder()
+        function pred = test_bilinear_prediction()
             encrypter = xorEncrypter;
             encoder = Encoder(encrypter);
-            [result, key1, ~] = encoder.apply();
+            [result, imageKey, ~, coefficient] = encoder.apply();
             predictor = bilinearPredictor;
             decoder = Decoder(encrypter, predictor, 0);
-            data = decoder.apply(result, key1);
+            pred = decoder.apply(result, coefficient, imageKey);
+            Image.showImage('預測影像', pred);
         end
 
-        function test_encryption()
-
+        function pred = test_dpcm_prediction()
+            encrypter = xorEncrypter;
+            encoder = Encoder(encrypter);
+            [result, imageKey, ~, coefficient] = encoder.apply();
+            predictor = dpcmPredictor;
+            decoder = Decoder(encrypter, predictor, 0);
+            pred = decoder.apply(result, coefficient, imageKey);
+            Image.showImage('預測影像', pred);            
         end
 
         function data = test_dataHiding()
             encrypter = xorEncrypter;
             encoder = Encoder(encrypter);
-            [result, key1, key2] = encoder.apply();
+            [result, ~, key2, ~] = encoder.apply();
             decoder = Decoder(encrypter, 0, 0);
             data = decoder.apply(result, key2);
+            Image.writeData('data.txt', data);
         end
     end
 end
